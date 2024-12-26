7687b801c1a7b312abb408addc743071af05077a
asuarezaliano <70922956+asuarezaliano@users.noreply.github.com>
2024-12-26 14:51:41 -0300

    fix build

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,8 @@
 import TasksContainer from '@/components/Home/TasksContainer';
 import { taskService } from '@/services/tasks.service';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const tasks: Task[] = await taskService.getTasks();
   const labels: Record<string, Label> = await taskService.getLabels();
